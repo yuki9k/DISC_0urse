9b7911822fe760398f155125fc423bae0fd468f1
theoskogsberg <theo.skogsberg02@gmail.com>
2024-05-07 10:30:57 +0200

    Updates

--- JS/components/sharedComponents/header/header.js
+++ JS/components/sharedComponents/header/header.js
@@ -1,8 +1,8 @@
 function renderHeader() {
-    const header = document.createElement("header");
-    let wrapper = document.querySelector("#wrapper");
-    wrapper.appendChild(header);
-    header.innerHTML = `
+  const header = document.createElement("header");
+  let wrapper = document.querySelector("#wrapper");
+  wrapper.appendChild(header);
+  header.innerHTML = `
       <div class="menu">
           <div class="menu_icon_container">
               <div class="bar1"></div>
@@ -11,19 +11,78 @@ function renderHeader() {
           </div>   
           <div class="dropdown">
               <div class="dropdown_item">
+                <div class="room_title_one">
                   <div class="dropdown_title">Friends</div>
+                  <div class="add_friend_button">
+                    <div>+</div>
+                  </div>
+                </div>
                   <div class="dropdown_friends">
-                      <div class="dropdown_box"></div>
-                      <div class="dropdown_box"></div>
-                      <div class="dropdown_box"></div>
+                      <div class="dropdown_box_friends">
+                        <div class="friend_image"></div> 
+                        <div class="friend_username">User23840232312</div> 
+                      </div>
+                      <div class="dropdown_box_friends">
+                        <div class="friend_image"></div> 
+                        <div class="friend_username">User233444502</div> 
+                      </div>
+                      <div class="dropdown_box_friends">
+                        <div class="friend_image"></div> 
+                        <div class="friend_username">User2384092384902</div> 
+                      </div>
                   </div>
               </div>
-              <div class="dropdown_item">
+              <div class="dropdown_item_rooms">
                   <div class="dropdown_title">Rooms</div>
+                  <div class="dropdown_title_two">Public Rooms</div>
                   <div class="dropdown_rooms">
-                      <div class="dropdown_box"></div>
-                      <div class="dropdown_box"></div>
-                      <div class="dropdown_box"></div>
+                      <div class="dropdown_box_rooms">
+                        <p>Genre</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Genre</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Genre</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Genre</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Genre</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Genre</p>
+                      </div>
+                  </div>
+                  <div class="room_title_two">
+                    <div class="dropdown_title_two">Private Rooms</div>
+                    <div class="add_friend_button">
+                        <div>+</div>
+                    </div>
+                  </div>
+                  <div class="dropdown_rooms">
+                      <div class="dropdown_box_rooms">
+                        <p>Room</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Room</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Room</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Room</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Room</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Room</p>
+                      </div>
+                      <div class="dropdown_box_rooms">
+                        <p>Room</p>
+                      </div>
                   </div>
               </div>
           </div>
@@ -34,26 +93,22 @@ function renderHeader() {
           <div class="signup_button">Sign up</div> 
       </div>
     `;
-  
-    const menuIcon = document.querySelector(".menu_icon_container");
-    const dropdown = document.querySelector(".dropdown");
-    const loginButton = document.querySelector(".login_button");
-    const signupButton = document.querySelector(".signup_button");
-  
-    // Toggle dropdown menu on menu icon click
-    menuIcon.addEventListener("click", () => {
-      menuIcon.classList.toggle("change");
-      dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-    });
-  
-    // Display login form when login button is clicked
-    loginButton.addEventListener("click", () => {
-      renderLoginForm(); // Call renderLoginForm function
-    });
-  
-    // Display signup form when signup button is clicked
-    signupButton.addEventListener("click", () => {
-      renderSignupForm(); // Call renderSignupForm function
-    });
-  }
-  
+
+  const menuIcon = document.querySelector(".menu_icon_container");
+  const dropdown = document.querySelector(".dropdown");
+  const loginButton = document.querySelector(".login_button");
+  const signupButton = document.querySelector(".signup_button");
+
+  menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("change");
+    dropdown.classList.toggle("active");
+  });
+
+  loginButton.addEventListener("click", () => {
+    renderLoginForm(); 
+  });
+
+  signupButton.addEventListener("click", () => {
+    renderSignupForm(); 
+  });
+}
--- index.js
+++ index.js
@@ -1,2 +1,1 @@
 renderHeader();
-// renderLoginForm();
