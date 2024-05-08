a0219b922cab6139ae8ed3cc332b76fb9916d63b
Ronja <143420962+rvrdotter@users.noreply.github.com>
2024-05-08 14:11:14 +0200

    pfp uploads
    pfp uploader should work but needs alot of error handling added

--- API/pfp.php
+++ API/pfp.php
@@ -6,11 +6,13 @@ if($requestMethod != "POST"){
     sendError(400, "METHOD NOT ALLOWED");
 }
 $requestData = getRequestData();
-$dir = "./profilePictures";
+$dir = "./profilePictures/";
 //req token!!
 if($requestMethod == "POST"){
     $pfp = $_FILES["pfp"];
     $user = getUserFromToken($requestData["token"]);
-    move_uploaded_file($pfp["name"], $dir . "/" . $user["name"] . "_pfp.jpg");
+    $destination = $dir . $user["name"] . "_pfp.jpg";
+    $source = $pfp["tmp_name"];
+    move_uploaded_file($source, $destination);
 }
 ?>
