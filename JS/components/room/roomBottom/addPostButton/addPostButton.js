4d9b19c1dbc16f08a26d4df54cc936e86ff3eaf7
gnajor <lejonet04@gmail.com>
2024-05-13 22:44:01 +0200

    Added some animation to the textarea

--- JS/components/room/roomBottom/addPostButton/addPostButton.js
+++ JS/components/room/roomBottom/addPostButton/addPostButton.js
@@ -10,16 +10,35 @@ function renderAddPostButton(parent){
     const addPostButton = parent.querySelector("#add_post_button");
     const input = parent.querySelector("#post_input");
 
-    addPostButton.addEventListener("click", (event) => {
+    addPostButton.addEventListener("click", () => {
         plusSign.classList.add("undisplay");
         addPostButton.classList.add("border_rad_change");
 
         //width increase transtion and stuff
-        addPostButton.addEventListener("transitionend", (event) => {
-            input.classList.toggle("display");
-            plusSign.classList.toggle("display_none");
+        addPostButton.addEventListener("transitionend", () => {
+            if(input.className === "display"){
+                input.classList.remove("display");
+                plusSign.classList.remove("display_none");
+            }
+            else{
+                input.classList.add("display");
+                plusSign.classList.add("display_none");
+            }
         });
+
+        window.addEventListener("click", function onWindowClickCloseInput(event){
+            if(event.target !== addPostButton && event.target !== input){
+                input.classList.remove("display");
+                plusSign.classList.remove("display_none");
+
+                plusSign.classList.remove("undisplay");
+                addPostButton.classList.remove("border_rad_change");
+            
+            } 
+        });     
     });
+
+
 
 /*     window.addEventListener("click", (event) => {
         event.stopPropagation();
