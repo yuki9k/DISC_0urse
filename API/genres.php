f60bc99f528287077c9e4ddf01c681d3e4ebb950
yuki9k <tornlund.freja@gmail.com>
2024-05-14 13:42:56 +0200

    Fixed everything

--- API/genres.php
+++ API/genres.php
@@ -1,28 +1,34 @@
-<?php 
-require_once("auxFunctions.php");
-require_once("../php/httpHandlers.php");
+<?php
+require_once "auxFunctions.php";
+require_once "../php/httpHandlers.php";
 $allowedMethods = ["GET", "POST"];
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
-if(!in_array($requestMethod, $allowedMethods)){
+if (!in_array($requestMethod, $allowedMethods)) {
     sendError(400, "METHOD NOT ALLOWED");
 }
 $requestData = getRequestData();
-if($requestMethod == "POST"){
-    if(empty($requestData)){
+if ($requestMethod == "POST") {
+    if (empty($requestData)) {
         sendError(400, "bad request");
     }
-    file_put_contents("genres.json", $requestData);
-    $genres =  json_decode($requestData);
-    foreach($genre as $index => $genres){
-        $image = file_get_contents($genre["albumImages"][0]["url"], false);
-        file_put_contents("/albumPics/$index.jpeg", $image);
+    $encoded = json_encode(
+        $requestData,
+        JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT
+    );
+    file_put_contents("db/genres.json", $encoded);
+    foreach ($requestData as $genreName => $genreObject) {
+        $albumImageFile = file_get_contents(
+            $genreObject["albumImages"][0]["url"],
+            false
+        );
+        file_put_contents("db/albumPics/{$genreName}.jpeg", $albumImageFile);
     }
 }
-if($requestMethod == "GET"){
+if ($requestMethod == "GET") {
     //this method sends genre information to the client.
     $DBinfo = getDatabase("genres");
-    if(!$DBInfo){
+    if (!$DBInfo) {
         sendError("genre DB empty");
     }
     send(200, $DBInfo);
--- php/spotifyDemo.php
+++ php/spotifyDemo.php
@@ -5,8 +5,6 @@ require_once "demoAuth.php";
 require_once "apiCommunication.php";
 
 // echo var_dump($genresFile);
-// spotifyGetAllGenreAlbums($demoAuth);
+// $genresFile = spotifyGetAllGenreAlbums($demoAuth);
 $genresFile = file_get_contents("genres.json");
-$resJson = sendAlbumsToApi($genresFile);
-$res = json_decode($resJson, false);
-echo $resJson;
+sendAlbumsToApi($genresFile);
