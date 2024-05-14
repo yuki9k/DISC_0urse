800bad98e9b7e5d16320720c7b46240d6628750a
gnajor <lejonet04@gmail.com>
2024-05-14 10:04:39 +0200

    Removed index.js content

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-import * as homepage from "./JS/components/homepage/homepage.js";
-import { PubSub } from "./JS/logic/PubSub.js";
 
-PubSub.publish({
-    event: "renderHomepage",
-    details: document.querySelector("#wrapper")
-});
 
