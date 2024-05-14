a357f42d2612cb31c504ac5ade07a9e79527ab1b
theoskogsberg <theo.skogsberg02@gmail.com>
2024-05-14 10:11:04 +0200

    Links and imports
    Added style links and imported main

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import * as header from "./JS/components/sharedComponents/header/header.js";
+import * as main from "./JS/components/homepage/homepage.js";
+
 import { PubSub } from "./JS/logic/PubSub.js";
 
 PubSub.publish({
